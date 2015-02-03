50e833013e36773bf4cd7d4d934275a7fb077ee0
Anirudh Parui <anirudh@ambab.com>
2015-02-03 14:25:30 +0530

    Adding Source Files

---
+++ js/breakingBad.js
@@ -0,0 +1,126 @@
+angular.module('breakingBad', [])
+    .controller("bbCtrl", ['$scope', function ($scope) {
+        $scope.name = "Breaking Bad";
+        $scope.count = 0;
+        // Event handlers
+        $scope.onKeyPress = function ($event) {
+           switch($event.keyCode) {
+                case 37:
+                    $scope.count -= 1;
+                    break;
+                case 39:
+                    $scope.count += 1;
+                    break;
+                default:
+                    break;
+            }
+            if($scope.count < 0){
+                $scope.count = 0;
+            }
+        };
+
+    }])
+    .directive('ngBreakingBad', function () {
+        return{
+            restrict: "AE",
+            replace: true,
+            link: function (scope, element) {
+                
+                scope.$watch('[name , count]', function () {
+                    var names =  scope.name.split(" ");
+                    if(names.length >= 2){
+                        var first = breakBad(names[0], scope.count);
+                        var last = breakBad(names[1], scope.count);
+                        var html = "<div id='breaking-bad'><div id='bb-body'>"+
+                                       "<div class='name first'>"
+                                       +first+
+                                       "</div><div style='clear: both'></div><div class='name last'>"
+                                       +last+
+                                        "</div></div></div>";
+                        element.html(html);
+                    }else{
+                        element.html('');
+                    }
+                    
+                });
+
+                // Array of all Elements
+                var elements = ['Ac','Ag','Al','Am','Ar','As','At','Au',
+                                'B','Ba','Be','Bh','Bi','Bk','Br',
+                                'C','Ca','Cd','Ce','Cf','Cl','Cm','Cn','Co','Cr','Cs','Cu',
+                                'Db','Ds','Dy',
+                                'Er','Es','Eu',
+                                'F','Fe','Fl','Fm','Fr',
+                                'Ga','Gd','Ge',
+                                'H','He','Hf','Hg','Ho','Hs',
+                                'I','In','Ir',
+                                'K','Kr',
+                                'La','Li','Lr','Lu','Lv',
+                                'Md','Mg','Mn','Mo','Mt',
+                                'N','Na','Nb','Nd','Ne','Ni','No','Np',
+                                'O','Os',
+                                'P','Pa','Pb','Pd','Pm','Po','Pr','Pt','Pu',
+                                'Ra','Rb','Re','Rf','Rg','Rh','Rn','Ru',
+                                'S','Sb','Sc','Se','Sg','Si','Sm','Sn','Sr',
+                                'Ta','Tb','Tc','Te','Th','Ti','Tl','Tm',
+                                'U','Uuo','Uup','Uus','Uut',
+                                'V',
+                                'W',
+                                'Xe',
+                                'Y','Yb',
+                                'Zn','Zr'];
+
+                // Search the element in the name
+                function breakBad(name, num){
+                    name = name.toLowerCase();
+                    if(name.length < 3){
+                        return '';
+                    }
+                    
+                    var userElements = [];
+                    r = name;
+                    for(i=0; i<elements.length; i++){
+
+                        // Let's not match single character elements.
+                        /*if(elements[i].length < 2) {
+                            continue;
+                        }*/
+
+                        // Does this element occur?
+                        symbol = elements[i].toLowerCase(),
+                        index = name.indexOf(symbol);
+
+                        // Nope!
+                        if(index === -1) {
+                            continue;
+                        } else if(num > 0) {
+                            num -= 1;
+                            continue;
+                        }
+
+                        // Yep!
+                        //userElements.push(this._elements[i].name);
+                        r = generate(name, elements[i] , index);
+                        break;
+                    }
+                    if(r == name){
+                        r = breakBad(name, num-1);
+                    }
+
+                    return r;
+
+
+                }
+
+                // Genrate the required HTML o/p
+                function generate(text, symbol, imdex){
+                    return "<span class='left'>"+text.slice(0, index)+ "</span>" +
+                           "<img class='element' src='elements/" + symbol + ".png'>" + 
+                           "<span class='right'>" + text.slice(index + symbol.length, text.length) +
+                           "</span>";
+                }
+
+                //console.log($scope.name);
+            }
+        };
+    })
